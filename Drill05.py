bf0a0bee83fa9e0cc9403cd9b147165520c5c678
2015180020-2dgp <rhshrhsh96@gmail.com>
2018-09-17 16:28:37 +0900

    drill05-move from left to center함수 완성
    move from left to center 함수 완성 및
    실행 성공, make circle 함수 중복 제거

--- Drill05.py
+++ Drill05.py
@@ -42,7 +42,13 @@ def move_down():
         delay(0.01)
 
 def move_from_left_to_center():
-    pass
+    x, y = 2, 90
+    while x < 400:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
 
 
 
@@ -50,16 +56,11 @@ def make_rectangle():
     # move_from_center_to_right()
     # move_up()
     # move_left()
-    move_down()
+    # move_down()
     move_from_left_to_center()
 
 def make_circle():
     pass
-
-
-def make_circle():
-    pass
-
 
 while True:
     make_rectangle()
