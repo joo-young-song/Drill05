799a5393414a786d5b2fed748e171d01516b1673
2015180020-2dgp <rhshrhsh96@gmail.com>
2018-09-17 16:13:06 +0900

    drill05시작
    drill05의 시작

--- Drill05.py
+++ Drill05.py
@@ -5,35 +5,6 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def move_from_center_to_right():
-    pass
-
-def move_up():
-    pass
-
-def move_left():
-    pass
-
-def move_down():
-    pass
-
-def move_from_left_to_center():
-    pass
-
-
-
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
-
-def make_circle():
-    pass
-
-while True:
-    make_rectangle()
-    make_circle()
+# fill here
     
 close_canvas()
