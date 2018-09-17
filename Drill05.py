7deb6a32fa1a18d2c85877c3f19b9df0f55fbace
2015180020-2dgp <rhshrhsh96@gmail.com>
2018-09-17 16:22:05 +0900

    drill05-up 함수 완성
    up함수 내부를 채우고 실행에 성공하였다.

--- Drill05.py
+++ Drill05.py
@@ -6,7 +6,7 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 def move_from_center_to_right():
-    x,y = 800 // 2, 90
+    x,y = 2, 90
     while x < 800 - 25 :
         clear_canvas_now()
         grass.draw_now(400,30)
@@ -15,6 +15,13 @@ def move_from_center_to_right():
         delay(0.01)
 
 def move_up():
+    x, y = 800 - 25 , 90
+    while y < 600 - 25:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
     pass
 
 def move_left():
@@ -29,7 +36,7 @@ def move_from_left_to_center():
 
 
 def make_rectangle():
-    move_from_center_to_right()
+    # move_from_center_to_right()
     move_up()
     move_left()
     move_down()
