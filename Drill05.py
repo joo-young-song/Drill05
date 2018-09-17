2a5db716866a8be3f255e803fa84ad2502912a31
2015180020-2dgp <rhshrhsh96@gmail.com>
2018-09-17 17:17:28 +0900

    drill05-715 to 316 clear
    이동함수를 완성하고 실행성공

--- Drill05.py
+++ Drill05.py
@@ -85,7 +85,24 @@ def run_477_to_715():
             y -= 2
             delay(0.05)
 def run_715_to_316():
-    pass
+    x, y, frame = 715, 136, 0
+    while y < 225:
+        if x > 316:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character_run.clip_draw(frame * 100, 0, 100, 100, x, y)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x -= 2
+            delay(0.05)
+        else:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character_run.clip_draw(frame * 100, 0, 100, 100, x, y)
+            update_canvas()
+            frame = (frame + 1) % 8
+            y += 2
+            delay(0.05)
 def run_316_to_510():
     pass
 def run_510_to_692():
