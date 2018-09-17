572649fdf78b3e9431db47032905e01c86440a58
2015180020-2dgp <rhshrhsh96@gmail.com>
2018-09-17 16:26:25 +0900

    drill05-down 함수 완성
    down 함수 완성 및 실행 성공
    move from center to right 함수  x좌표 시작위치 변경 완료 및 실행 성공

--- Drill05.py
+++ Drill05.py
@@ -6,7 +6,7 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 def move_from_center_to_right():
-    x,y = 2, 90
+    x,y = 400, 90
     while x < 800 - 25 :
         clear_canvas_now()
         grass.draw_now(400,30)
@@ -25,7 +25,7 @@ def move_up():
 
 def move_left():
     x, y = 800 - 25, 600 - 25
-    while x > 0 + 25:
+    while x > 0 + 2:
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, y)
@@ -33,7 +33,13 @@ def move_left():
         delay(0.01)
 
 def move_down():
-    pass
+    x, y = 2, 600 - 25
+    while y > 0 + 90:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y -= 2
+        delay(0.01)
 
 def move_from_left_to_center():
     pass
@@ -43,7 +49,7 @@ def move_from_left_to_center():
 def make_rectangle():
     # move_from_center_to_right()
     # move_up()
-    move_left()
+    # move_left()
     move_down()
     move_from_left_to_center()
 
